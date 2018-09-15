b88083706628189f9034c652854dc5a110b2e3d0
Joe Previte <jjprevite@gmail.com>
2018-09-15 11:24:15 -0700

    add function to end of lesson

--- send-text-node-twilio/index.js
+++ send-text-node-twilio/index.js
@@ -2,3 +2,12 @@ require('dotenv').load()
 const accountSid = process.env.ACCOUNT_SID
 const authToken = process.env.AUTH_TOKEN
 const client = require('twilio')(accountSid, authToken)
+
+client.messages
+  .create({
+    body: 'Hello from the world of Node!',
+    from: process.env.TWILIO_NUMBER,
+    to: process.env.PHONE_NUMBER
+  })
+  .then(() => console.log('success!'))
+  .done()
