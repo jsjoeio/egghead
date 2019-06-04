7102fc73ad5ba13ead57c97efad7211aebdace42
Joe Previte <jjprevite@gmail.com>
2019-06-03 20:18:04 -0700

    Add use template literals

---
+++ use-template-literals/index.js
@@ -0,0 +1,11 @@
+function helloFromSpace (planet) {
+  return `Hello from, ${planet}`
+}
+
+console.log(helloFromSpace('Mars'))
+
+function addForMe (a, b) {
+  return `The sum of these two is: ${a + b}`
+}
+
+console.log(addForMe(3, 4))
