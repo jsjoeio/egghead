2b21b6475ba07d9ff217371be5e9873a72439469
JavaScript Joe <jjprevite@gmail.com>
2019-09-30 18:40:53 -0700

    feat: write custom effect hook

--- write-custom-effect-hook/average-react-app/src/components/Feedback/Feedback.custom.js
+++ write-custom-effect-hook/average-react-app/src/components/Feedback/Feedback.custom.js
@@ -4,21 +4,38 @@ import { Form, Label, Textarea, Button, Title } from './Feedback.styles'
 function useText(initialValue) {
   return useState(initialValue)
 }
-export function FeedbackCustomComponent() {
-  const [text, setText] = useText('')
+
+function useStarWarsQuote() {
+  const [quote, setQuote] = useState('')
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
     async function getStarWarsQuote() {
+      setLoading(true)
       // Get initial text
       const response = await fetch(
         'http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote'
       )
       const data = await response.json()
       const quote = data.starWarsQuote
-      setText(quote)
+      setQuote(quote)
+      setLoading(false)
     }
     getStarWarsQuote()
   }, [])
 
+  return { quote, loading }
+}
+
+export function FeedbackCustomComponent() {
+  const [text, setText] = useText('')
+  const { quote, loading } = useStarWarsQuote()
+
+  useEffect(() => {
+    if (quote) {
+      setText(quote)
+    }
+  }, [quote, setText])
   // Handle form submission
   function handleSubmit(e) {
     e.preventDefault()
@@ -33,14 +50,20 @@ export function FeedbackCustomComponent() {
     setText(updatedText)
   }
 
-  return (
-    <Form onSubmit={e => handleSubmit(e)}>
-      <Title>Custom Example</Title>
-      <Label>
-        Have feedback for our team? <br /> Let us know here 👇
+  if (loading) return <p>Loading...</p>
+
+  if (quote) {
+    return (
+      <Form onSubmit={e => handleSubmit(e)}>
+        <Title>Custom Example</Title>
+        <Label>
+          Have feedback for our team? <br /> Let us know here 👇
         <Textarea value={text} onChange={e => handleTextChange(e)} />
-      </Label>
-      <Button type="submit">Submit</Button>
-    </Form>
-  )
+        </Label>
+        <Button type="submit">Submit</Button>
+      </Form>
+    )
+  }
+
+  return null
 }
