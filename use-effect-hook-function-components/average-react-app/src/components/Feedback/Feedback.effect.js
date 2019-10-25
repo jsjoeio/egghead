4347ec4cec268e2c867f89bd95a33d4830ecaa28
Zac Jones <zacjones93@gmail.com>
2019-10-25 17:06:52 -0400

    Replace api url with HTTPS proxy

--- use-effect-hook-function-components/average-react-app/src/components/Feedback/Feedback.effect.js
+++ use-effect-hook-function-components/average-react-app/src/components/Feedback/Feedback.effect.js
@@ -7,7 +7,7 @@ export function FeedbackEffectComponent() {
     async function getStarWarsQuote() {
       // Get initial text
       const response = await fetch(
-        'http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote'
+        'https://starwars-quote-proxy-gi0d3x1lz.now.sh/api/randomQuote'
       )
       const data = await response.json()
       const quote = data.starWarsQuote
